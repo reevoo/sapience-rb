e42870b133f79446e8cfe3158c57902bc72c210a
Mikael Henriksson <mikael@zoolutions.se>
2016-08-05 11:30:51 +0200

    Add tests for loggable module

--- spec/lib/sapience/loggable_spec.rb
+++ spec/lib/sapience/loggable_spec.rb
@@ -1,24 +1,100 @@
 require "spec_helper"
+require "stringio"
+class TestAttribute
+  include Sapience::Loggable
+end
+
+module Perform
+  def perform
+    logger.info("perform")
+  end
+end
+
+class Base
+  include Sapience::Loggable
+  include Perform
+end
+
+module Process
+  def process
+    logger.info("process")
+  end
+end
+
+class Subclass < Base
+  include Process
+end
 
 describe Sapience::Loggable do
-  class TestClass
+  let(:called_proc) { -> description { called = true if description == "perform" } }
+  describe "inheritance" do
+    it "should give child classes their own logger" do
+      expect(Subclass.logger.name).to eq(Subclass.name)
+      expect(Base.logger.name).to eq(Base.name)
+      expect(Subclass.logger.name).to eq(Subclass.name)
+      child_logger = Subclass.logger
+      expect(Base.logger).to_not eq(child_logger)
+      expect(Subclass.logger.object_id).to eq(child_logger.object_id)
+    end
+
+    it "should give child objects their own logger" do
+      subclass = Subclass.new
+      base = Base.new
+      expect(subclass.logger.name).to eq(subclass.class.name)
+      expect(base.logger.name).to eq(base.class.name)
+      expect(subclass.logger.name).to eq(subclass.class.name)
+      child_logger = subclass.logger
+      expect(base.logger).to_not eq(child_logger)
+      expect(subclass.logger.object_id).to eq(child_logger.object_id)
+    end
+
+    it "should allow mixins to call parent logger" do
+      base = Base.new
+      expect(Base.logger).to receive(:info).with("perform")
+      base.perform
+    end
+
+    it "should allow child mixins to call parent logger" do
+      subclass = Subclass.new
+      expect(Subclass.logger).to receive(:info).with("process")
+      subclass.process
+    end
   end
 
-  describe "when included" do
-    before(:all) do
-      TestClass.send(:include, described_class) unless TestClass.ancestors.include?(described_class)
+  describe "logger" do
+    before do
+      @time = Time.new
+      @io = StringIO.new
+      @appender = Sapience::Appender::File.new(@io)
+      Sapience.default_level = :trace
+      @mock_logger = MockLogger.new
+      @appender = Sapience.add_appender(logger: (@mock_logger))
+      @hash = { session_id: "HSSKLEU@JDK767", tracking_number: 12345 }
+      @hash_str = @hash.inspect.sub("{", "\\{").sub("}", "\\}")
+      @thread_name = Thread.current.name
     end
 
-    describe "class methods" do
-      subject { TestClass }
-      specify { expect(subject).to respond_to(:logger) }
-      specify { expect(subject).to respond_to(:logger=) }
-    end
+    after { Sapience.remove_appender(@appender) }
 
-    describe "instance methods" do
-      subject { TestClass.new }
-      specify { expect(subject).to respond_to(:logger) }
-      specify { expect(subject).to respond_to(:logger=) }
+    describe "for each log level" do
+      Sapience::LEVELS.each do |level|
+        it "log #{level} information with class attribute" do
+          allow(Sapience).to receive(:backtrace_level_index).and_return(0)
+          allow(Sapience).to receive(:appenders).and_return([@appender])
+          TestAttribute.logger.send(level, "hello #{level}", @hash)
+          Sapience.flush
+          expect(@mock_logger.message).to match(/#{TS_REGEX} \w \[\d+:#{@thread_name} example.rb:\d+\] TestAttribute -- hello #{level} -- #{@hash_str}/)
+        end
+
+        it "log #{level} information with instance attribute" do
+          allow(Sapience).to receive(:backtrace_level_index).and_return(0)
+          allow(Sapience).to receive(:appenders).and_return([@appender])
+          TestAttribute.new.logger.send(level, "hello #{level}", @hash)
+          Sapience.flush
+          expect(@mock_logger.message).to match(/#{TS_REGEX} \w \[\d+:#{@thread_name} example.rb:\d+\] TestAttribute -- hello #{level} -- #{@hash_str}/)
+
+        end
+      end
     end
   end
 end
