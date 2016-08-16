609888fc3900e35866fcbd90284cb664c9973be9
Mikael Henriksson <mikael@zoolutions.se>
2016-08-16 16:56:52 +0200

    Prepare for more statsd appenders

--- lib/sapience/appender/datadog.rb
+++ lib/sapience/appender/datadog.rb
@@ -1,6 +1,6 @@
 require "uri"
 begin
-  require "statsd-ruby"
+  require "datadog"
 rescue LoadError
   raise 'Gem statsd-ruby is required for logging metrics. Please add the gem "statsd-ruby" to your Gemfile.'
 end
@@ -10,7 +10,7 @@ end
 #
 module Sapience
   module Appender
-    class Statsd < Sapience::Subscriber
+    class Datadog < Sapience::Appender::Statsd
       # Create Appender
       #
       # Parameters:
@@ -24,20 +24,19 @@ module Sapience
       # rubocop:disable AbcSize, CyclomaticComplexity, PerceivedComplexity
       def initialize(options = {}, &block)
         options = options.is_a?(Hash) ? options.dup : { level: options }
-        url     = options.delete(:url) { "udp://localhost:8125" }
-        @uri    = URI.parse(url)
-        fail('Statsd only supports udp. Example: "udp://localhost:8125"') if @uri.scheme != "udp"
+        url     = options.delete(:url) || "udp://localhost:8125"
+        uri     = URI.parse(url)
+        fail('Statsd only supports udp. Example: "udp://localhost:8125"') if uri.scheme != "udp"
+
+
+        path              = uri.path.chomp("/")
+        @statsd.namespace = path.sub("/", "") if path != ""
 
         super(options, &block)
       end
 
       def provider
-        @_provider ||= begin
-          statsd = ::Statsd.new(@uri.host, @uri.port)
-          path   = @uri.path.chomp("/")
-          statsd.namespace = path.sub("/", "") if path != ""
-          statsd
-        end
+        @_provider ||= ::Statsd.new(uri.host, uri.port)
       end
 
       # Send an error notification to sentry
@@ -59,18 +58,18 @@ module Sapience
       end
 
       def timing(metric, duration)
-        provider.timing(metric, duration)
+        @statsd.timing(metric, duration)
       end
 
       def increment(metric, amount)
-        provider.batch do
-          amount.times { provider.increment(metric) }
+        @statsd.batch do
+          amount.times { @statsd.increment(metric) }
         end
       end
 
       def decrement(metric, amount)
-        provider.batch do
-          amount.abs.times { provider.decrement(metric) }
+        @statsd.batch do
+          amount.abs.times { @statsd.decrement(metric) }
         end
       end
     end
