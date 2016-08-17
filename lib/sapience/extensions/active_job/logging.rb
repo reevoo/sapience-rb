dda7082e620fbb27598d1bc035a4f62b7ea3a72d
Mikael Henriksson <mikael@zoolutions.se>
2016-08-17 11:32:48 +0200

    Comment out overridden method

--- lib/sapience/extensions/active_job/logging.rb
+++ lib/sapience/extensions/active_job/logging.rb
@@ -4,10 +4,11 @@ require "active_job/logging"
 module ActiveJob::Logging # rubocop:disable ClassAndModuleChildren
   include Sapience::Loggable
 
-  private
+  # private
 
-  alias_method :tag_logger_old, :tag_logger
-  def tag_logger(*tags, &block)
-    logger.tagged(*tags, &block)
-  end
+  # alias_method :tag_logger_old, :tag_logger
+
+  # def tag_logger(*tags, &block)
+  #   logger.tagged(*tags, &block)
+  # end
 end
