c9c8566234878645536112903dbffba8bb5f2024
Mikael Henriksson <mikael@zoolutions.se>
2016-08-18 18:25:08 +0200

    [+AM] Adds coverage for Sapience module

---
+++ spec/lib/sapience/sapience_spec.rb
@@ -0,0 +1,97 @@
+require "spec_helper"
+
+describe Sapience do
+  describe ".config" do
+    subject(:config) { described_class.config }
+    context "returns 'default' when no environment is provided" do
+      before { allow(described_class).to receive(:environment).and_return(nil) }
+      its(:default_level) do
+        is_expected.to eq(:info)
+      end
+    end
+  end
+
+  describe ".environment" do
+    subject { described_class }
+
+    context "when RAILS_ENV is set" do
+      let(:env) { "integration" }
+      before { allow(ENV).to receive(:fetch).with("RAILS_ENV").and_return(env) }
+
+      its(:environment) do
+        is_expected.to eq(env)
+      end
+    end
+
+    context "when RACK_ENV is set" do
+      let(:env) { "ci" }
+      before do
+        allow(ENV).to receive(:fetch).with("RAILS_ENV").and_call_original
+        allow(ENV).to receive(:fetch).with("RACK_ENV").and_return(env)
+      end
+
+      its(:environment) do
+        is_expected.to eq(env)
+      end
+    end
+
+    context "when Rails respond to .env " do
+      let(:env) { "fudge" }
+      before do
+        allow(Rails).to receive(:env).and_return(env)
+      end
+
+      its(:environment) do
+        is_expected.to eq(env)
+      end
+    end
+  end
+
+  describe ".configure" do
+    let(:file_options) do
+      { io: STDOUT }
+    end
+    let(:file_appender) do
+      { file: file_options }
+    end
+    let(:sentry_options) do
+      { dsn: "https://getsentry.com:443" }
+    end
+    let(:sentry_appender) do
+      { sentry: sentry_options }
+    end
+    let(:appenders) { [file_appender, sentry_appender] }
+
+    before do
+      allow(described_class.config).to receive(:appenders).and_return(appenders)
+      expect(described_class).to receive(:add_appender).with(:file, file_options)
+      expect(described_class).to receive(:add_appender).with(:sentry, sentry_options)
+    end
+
+    context "when provided a block" do
+      it "adds all configured appenders" do
+        described_class.configure do |config|
+          expect(config).to be_a(Sapience::Configuration)
+        end
+      end
+    end
+
+    context "when no block given" do
+      it "adds all configured appenders" do
+        expect(described_class.configure).to be_a(Sapience::Configuration)
+      end
+    end
+  end
+
+  describe ".logger" do
+    specify do
+      expect(described_class.logger).to be_a(Sapience::Appender::File)
+    end
+  end
+
+  describe ".metrix" do
+    specify do
+      expect(described_class.metrix).to be_a(Sapience::Appender::Datadog)
+    end
+  end
+end
