769f9f513ed6ec7da55a4f5b1756eef68c3e957d
Mikael Henriksson <mikael@zoolutions.se>
2016-08-19 10:36:43 +0200

    Add docker compose files for linux debugging

---
+++ dev-entrypoint.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# The Docker App Container's development entrypoint.
+# This is a script used by the project's Docker development environment to
+# setup the app containers and databases upon runnning.
+set -e
+
+# 5: Check or install the app dependencies via Bundler:
+bundle check || bundle install --jobs 8 --retry 5
+exec "$@"
