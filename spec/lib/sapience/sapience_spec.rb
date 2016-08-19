2cacad6eb53dc40412c6cba4894494495843b572
Mikael Henriksson <mikael@zoolutions.se>
2016-08-19 10:45:16 +0200

    [+AM] Move specs to the right spec file

--- spec/lib/sapience/sapience_spec.rb
+++ spec/lib/sapience/sapience_spec.rb
@@ -1,8 +1,76 @@
 require "spec_helper"
 
 describe Sapience do
+  describe ".add_appender" do
+    subject(:add_appender) { described_class.add_appender(appender, options) }
+
+    after do
+      Sapience.remove_appender(subject) if options.is_a?(Hash)
+    end
+
+    context "when options is not a Hash" do
+      let(:appender) { :file }
+      let(:options) { 1 }
+      specify { expect { add_appender }.to raise_error(ArgumentError, "options should be a hash") }
+    end
+
+    context "when appender is :file" do
+      let(:appender) { :file }
+
+      context "and options has :io key present" do
+        let(:options) do
+          {
+            io:        STDOUT,
+            formatter: :color,
+          }
+        end
+
+        it { is_expected.to be_a(Sapience::Appender::File) }
+        its(:formatter) { is_expected.to be_a(Sapience::Formatters::Color) }
+      end
+
+      context "and options has :file_name key present" do
+        let(:options) do
+          {
+            file_name: "log/sapience.log",
+            formatter: :json,
+          }
+        end
+
+        it { is_expected.to be_a(Sapience::Appender::File) }
+        its(:formatter) { is_expected.to be_a(Sapience::Formatters::Json) }
+      end
+    end
+
+    context "when :statsd key is present" do
+      let(:appender) { :datadog }
+      let(:options) do
+        {
+          url: "udp://localhost:2222",
+        }
+      end
+
+      it { is_expected.to be_a(Sapience::Appender::Datadog) }
+    end
+
+    context "when :sentry key is present" do
+      let(:appender) { :sentry }
+      let(:options) do
+        {
+          level: :info,
+          dsn: "https://foobar:443",
+        }
+      end
+
+      it { is_expected.to be_a(Sapience::Appender::Sentry) }
+    end
+  end
+
   describe ".config" do
     subject(:config) { described_class.config }
+
+    it { is_expected.to be_a(Sapience::Configuration) }
+
     context "returns 'default' when no environment is provided" do
       before { allow(described_class).to receive(:environment).and_return(nil) }
       its(:default_level) do
