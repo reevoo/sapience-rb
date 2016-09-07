dd0d09fc494b8fd8b23f0868102a37266b36aaf6
Alex Malkov <alex.malkov@reevoo.com>
2016-09-07 16:52:53 +0100

    Add initial test for ActionView::LogSubscriber

---
+++ spec/lib/sapience/extensions/action_view/log_subscriber_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+require "sapience/extensions/action_view/log_subscriber"
+
+describe Sapience::Extensions::ActionView::LogSubscriber do
+  describe "#info" do
+    let(:message) { "Test Message" }
+    let(:logger) { Sapience[described_class] }
+
+    before do
+      allow(subject).to receive(:logger).and_return(logger)
+    end
+
+    specify do
+      expect(logger).to receive(:debug).with(message).and_yield
+      subject.info(message) do
+        1 + 1
+      end
+    end
+  end
+end
