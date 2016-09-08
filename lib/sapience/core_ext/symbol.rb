c192eea992654fa4b3675502701b973fa67621c8
Mikael Henriksson <mikael@zoolutions.se>
2016-09-08 12:10:43 +0200

    [+AM] Fix style violations

--- lib/sapience/core_ext/symbol.rb
+++ lib/sapience/core_ext/symbol.rb
@@ -1,15 +1,15 @@
 class Symbol
   def camelize(uppercase_first_letter = true)
-    string = self.to_s
+    string = to_s
     string = if uppercase_first_letter
                string.sub(/^[a-z\d]*/) { $&.capitalize }
              else
                string.sub(/^(?:(?=\b|[A-Z_])|\w)/) { $&.downcase }
              end
-    string.gsub!(%r{(?:_|(\/))([a-z\d]*)}i) do
+    string.gsub!(/(?:_|(\/))([a-z\d]*)/i) do
       "#{Regexp.last_match(1)}#{Regexp.last_match(2).capitalize}"
     end
-    string.gsub!(%r{/}, '::')
+    string.gsub!(/\//, "::")
     string
-  end #unless :sym.respond_to?(:camelize)
+  end # unless :sym.respond_to?(:camelize)
 end
