e8c00fff722fdf0e7c357038463980cb2abdfa9f
Mikael Henriksson <mikael@zoolutions.se>
2016-09-09 16:14:32 +0200

    Only require when ams is loaded

--- lib/sapience/extensions/active_model_serializers/logging.rb
+++ lib/sapience/extensions/active_model_serializers/logging.rb
@@ -1,19 +1,21 @@
 # Patch ActiveModelSerializers logger
-require "active_model_serializers"
-require "active_model_serializers/logging"
 
-module ActiveModelSerializers::Logging # rubocop:disable ClassAndModuleChildren
-  def self.included(base)
-    base.send(:include, Sapience::Loggable)
+if defined?(ActiveModelSerializers)
+  require "active_model_serializers/logging"
+
+  module ActiveModelSerializers::Logging # rubocop:disable ClassAndModuleChildren
+    def self.included(base)
+      base.send(:include, Sapience::Loggable)
+    end
+
+    private
+
+    alias_method :orig_tag_logger, :tag_logger
+
+    def tag_logger(*tags, &block)
+      logger.tagged(*tags, &block)
+    end
   end
 
-  private
-
-  alias_method :orig_tag_logger, :tag_logger
-
-  def tag_logger(*tags, &block)
-    logger.tagged(*tags, &block)
-  end
+  ActiveModelSerializers.send(:include, Sapience::Loggable)
 end
-
-ActiveModelSerializers.send(:include, Sapience::Loggable)
