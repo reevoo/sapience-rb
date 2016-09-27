a3e51cf176595ed278c1021fdfb45c78dc8822bf
Mika Hel <mikael@zoolutions.se>
2016-09-27 10:02:54 +0100

    [+AM] Merge application configs into default

--- lib/sapience/config_loader.rb
+++ lib/sapience/config_loader.rb
@@ -17,18 +17,28 @@ module Sapience
     DEFAULT_FILE  = File.join(SAPIENCE_HOME, "config", "default.yml")
 
     def self.load_from_file
-      file_path = config_file_path
-      path = File.absolute_path(file_path)
-      load_yaml_configuration(path)
+      default_config     = load_yaml_configuration(config_file_path)
+      application_config = load_yaml_configuration(application_config_file)
+      merge_configs(default_config, application_config)
+    end
+
+    def self.merge_configs(left_config = {}, right_config = {})
+      left_config.each do |key, config = {}|
+        right = right_config.fetch(key) { Hash.new }
+        config.merge!(right)
+      end
+
+      (right_config.keys - left_config.keys).each do |left_over_key|
+        left_config[left_over_key] = right_config[left_over_key]
+      end
+      left_config
     end
 
     class << self
       private
 
       def config_file_path
-        return application_config_file if File.exist?(application_config_file)
-
-        DEFAULT_FILE
+        File.absolute_path(DEFAULT_FILE)
       end
 
       def application_config_file
@@ -44,6 +54,7 @@ module Sapience
       end
 
       def load_yaml_configuration(absolute_path)
+        return {} unless File.exist?(absolute_path)
         text      = IO.read(absolute_path, encoding: "UTF-8")
         erb       = ERB.new(text)
         yaml_code = erb.result
--- spec/lib/sapience/config_loader_spec.rb
+++ spec/lib/sapience/config_loader_spec.rb
@@ -78,15 +78,55 @@ describe Sapience::ConfigLoader do
 
         it "uses the default configuration" do
           expect(load_from_file).to eq(
-            "development" => {
-              "log_level" => "debug",
-              "appenders" => [{
-                "stream" => {
-                  "io" => "STDOUT",
-                  "formatter" => "json",
+            "default" => {
+              "log_executor" => "single_thread_executor",
+              "log_level" => "info",
+              "appenders" => [
+                {
+                  "stream" => {
+                    "io" => "STDOUT",
+                    "formatter" => "color",
+                  },
                 },
-              }],
-            })
+              ],
+            },
+            "development" => {
+              "log_executor" => "single_thread_executor",
+              "log_level" => "debug",
+              "appenders" => [
+                {
+                  "stream" => {
+                    "io" => "STDOUT",
+                    "formatter" => "json",
+                  },
+                },
+              ],
+            },
+            "production" => {
+              "log_executor" => "single_thread_executor",
+              "log_level" => "warn",
+              "appenders" => [
+                {
+                  "stream" => {
+                    "file_name" => "log/production.log",
+                    "formatter" => "json",
+                  },
+                },
+              ],
+            },
+            "test" => {
+              "log_executor" => "immediate_executor",
+              "log_level" => "warn",
+              "appenders" => [
+                {
+                  "stream" => {
+                    "file_name" => "log/test.log",
+                    "formatter" => "color",
+                  },
+                },
+              ],
+            },
+          )
         end
         after { delete_file("config/sapience.yml") }
       end
@@ -97,6 +137,53 @@ describe Sapience::ConfigLoader do
         before { hide_const("Rack::Directory") }
         it_behaves_like "loading application configuration"
       end
+    end
+  end
+
+  describe ".merge_configs" do
+    let(:left_config) do
+      {
+        "default" => {
+          "key" => "val",
+          "key2" => "val2",
+        },
+        "development" => {
+          "key" => "val",
+          "key2" => "val",
+        },
+      }
+    end
+    let(:right_config) do
+      {
+        "development" => {
+          "key" => "val",
+          "key2" => "val2",
+          "key3" => "val3",
+        },
+        "rspec" => {
+          "key" => "val",
+          "key2" => "val2",
+        },
+      }
+    end
+
+    subject(:merge_configs) { described_class.merge_configs(left_config, right_config) }
+    specify do
+      expect(merge_configs).to match(
+        "default" => {
+          "key" => "val",
+          "key2" => "val2",
+        },
+        "development" => {
+          "key" => "val",
+          "key2" => "val2",
+          "key3" => "val3",
+        },
+        "rspec" => {
+          "key" => "val",
+          "key2" => "val2",
+        },
+      )
     end
   end
 end
