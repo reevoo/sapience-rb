33b8e889a282f0498e967041ad88a5b9b939aa2b
Mika Hel <mikael@zoolutions.se>
2016-09-29 13:36:12 +0100

    [+AM] Try to cache all gems

--- dev-entrypoint.sh
+++ dev-entrypoint.sh
@@ -6,7 +6,7 @@
 set -e
 
 # 5: Check or install the app dependencies via Bundler:
-bundle check || bundle install --jobs 8 --retry 5
+bundle check --path ../../vendor/bundle || bundle install --jobs 8 --retry 5
 
 if [ -f config/database.yml ]; then
   bundle exec rake db:create db:migrate db:test:prepare
