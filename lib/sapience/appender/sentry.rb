08b87b54d73be4bff11371b5bea751dd7b1f1136
Mika Hel <mikael@zoolutions.se>
2016-10-13 15:46:11 +0200

    Fix a problem logging messages to sentry
    According to https://docs.sentry.io/clients/ruby/context/ it should be named `:extra`.

--- lib/sapience/appender/sentry.rb
+++ lib/sapience/appender/sentry.rb
@@ -68,7 +68,7 @@ module Sapience
           message = {
             error_class:   context.delete(:name),
             error_message: context.delete(:message),
-            context: context,
+            extra: context,
           }
           message[:backtrace] = log.backtrace if log.backtrace
           Raven.capture_message(message[:error_message], message)
